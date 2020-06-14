cafbc345a10b2f7f075fd9bf6b40818818948a94
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-14 15:13:26 +0800

    Add header to app

--- App.tsx
+++ App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from 'react-native';
+import { SafeAreaView, Text, View, } from 'react-native';
 import tailwind from "tailwind-rn"
 
 const App = () => (
-  <SafeAreaView style={tailwind('h-full')}>
-    <View style={tailwind('pt-12 items-center')}>
-      <View style={tailwind('bg-blue-200 px-3 py-1 rounded-full')}>
-        <Text style={tailwind('text-blue-800 font-semibold')}>
-          Hello Tailwind
-				</Text>
-      </View>
+  <View style={tailwind('pt-6')}>
+    <View style={tailwind('bg-purple-700')}>
+      <Text style={tailwind('m-4 text-white font-bold text-xl')}>React Native Scanner</Text>
     </View>
-  </SafeAreaView>
+  </View>
 );
 
 export default App
