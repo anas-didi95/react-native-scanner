220660ce45a05073ee7c17056a3d787367da7086
Anas Juwaidi <anas.didi95@gmail.com>
2020-06-14 15:23:01 +0800

    Add prettier config and script

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { SafeAreaView, Text, View, } from 'react-native';
+import React from "react"
+import { SafeAreaView, Text, View } from "react-native"
 import tailwind from "tailwind-rn"
-import Header from './src/components/Header';
+import Header from "./src/components/Header"
 
 const App: React.FC<{}> = () => (
-  <View style={tailwind('pt-6')}>
+  <View style={tailwind("pt-6")}>
     <Header />
   </View>
-);
+)
 
 export default App
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,10 @@ import tailwind from "tailwind-rn"
 import { View, Text } from "react-native"
 
 const Header: React.FC<{}> = () => (
-  <View style={tailwind('bg-purple-700')}>
-    <Text style={tailwind('my-4 mx-6 text-white font-bold text-xl')}>React Native Scanner</Text>
+  <View style={tailwind("bg-purple-700")}>
+    <Text style={tailwind("my-4 mx-6 text-white font-bold text-xl")}>
+      React Native Scanner
+    </Text>
   </View>
 )
 
